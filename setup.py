dd9e01abc49d12529a37dd14186d0455db934931
Thakur <jitendra.s.thakur@accenture.com>
2023-05-26 12:20:40 +0530

    Updated name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='Jeet',
+    name='jeet',
     version='0.0.1',
     author='Jitenra Thakur',
     author_email='jitendra@gmail.com',
