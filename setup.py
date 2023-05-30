650e97acbc0abc81c6e93da226a76bd16e70eb09
Thakur <jitendra.s.thakur@accenture.com>
2023-05-30 13:53:02 +0530

    updated setup read req

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 
 def req_parse(filename: str) -> list[str]:
+	print("REQ OUTPUT", TextFile(filename=str(Path(__file__).with_name(filename))).readlines())
     return TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
 
 
